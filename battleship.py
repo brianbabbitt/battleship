76aa0f83761aa7f6ae53298bcece4d8b6c59c2cb
Brian Babbitt <brianbabbitt@gmail.com>
2014-01-07 18:36:03 -0800

    added base 26 converter for larger sized maps

--- battleship.py
+++ battleship.py
@@ -14,12 +14,12 @@ from random import randint
 board = []
 ship_board =[]
 ship_locations = []
-row_headers = list(string.ascii_uppercase)
+column_headers = list(string.ascii_uppercase)
 
 ##---------- customizable options ----------##
 ships = [2,3,4,3,5]
 num_turns = 10
-difficulty = {'EASY':2.3,'MEDIUM':2.4,'HARD':2.7}
+difficulty = {'EASY':2.3,'MEDIUM':2.5,'HARD':2.8}
 
 ##---------- config parameters -------------##
 
@@ -43,6 +43,27 @@ def random_row(board):
 def random_col(board):
   return randint(0, len(board[0]) - 1)
 
+##----- convert column number to letter ----##
+def base_26_converter(column):
+  column = float(column)
+  answer = ""
+  list_of_remainders = []
+  if column > 26:
+    while column > 26:
+      if (column%26) == 0.0:
+        list_of_remainders.append((column%26))
+        column = int(math.floor(column/26)-1)
+      else:
+        list_of_remainders.append(column%26)
+        column = int(math.floor(column/26))
+  
+  list_of_remainders.append(column)
+  list_of_remainders.reverse()
+  
+  for column in list_of_remainders:
+    answer = answer + str(column_headers[int(column)-1])
+  return answer
+
 ##------ determine size of board -----------##
 def create_blank_game_board(level=difficulty['MEDIUM']):
   board_size = int(math.pow(max(ships),level)) ## start at max ship size
@@ -52,7 +73,7 @@ def create_blank_game_board(level=difficulty['MEDIUM']):
   while board_size < sum(ships)*2:
     logging.debug('Board size is to small: {}'.format(board_size))
     board_size+=1
-    
+  
   logging.debug('Final number of squares on board is: {}'.format(board_size))
   board_size = int(board_size**(.5))
   logging.debug('Final board length is: {}'.format(board_size))
@@ -67,7 +88,7 @@ def print_board(board):
   print '\n   ',
   for column in range(len(board)):
     i+=1
-    print row_headers[column],
+    print base_26_converter(i),
   print ''
   print '___'+('__'*i)
   i = 1
@@ -85,7 +106,7 @@ def check_ship(guess_row, guess_col):
       if ship_locations[x][y] == [guess_row, guess_col]:
         logging.debug('Ship is already present at this location ({},{})'.format(guess_row, guess_col))
         return True
-        
+  
   logging.debug('Location is available ({},{})'.format(guess_row, guess_col))
   return False
 
@@ -97,7 +118,7 @@ def auto_place_ship(ship_size, ship_number):
   ship_locations.append({})
   loop_count = 0
   loop_count2 = 0
-   
+  
   while ship_segments != 0:
     logging.debug('Number of remaining segments to place: {}'.format(ship_segments))
     
@@ -111,11 +132,11 @@ def auto_place_ship(ship_size, ship_number):
       else:
         logging.debug('Will attempt to pick different coords for ship.')
         continue
-        
+    
     ## second segment determines orientation
     elif ship_segments == (ship_size - 1):
       logging.debug('Placing second segment, determining orientation')
-        
+      
       temp_point = choice([{'dir':'row','loc':temp_row-1},{'dir':'row','loc':temp_row+1},{'dir':'col','loc':temp_col-1},{'dir':'col','loc':temp_col+1}])
       logging.debug('Random direction and new location: {} {}'.format(temp_point['dir'],temp_point['loc']))
       logging.debug('Checking if coords are on board...')
@@ -129,7 +150,7 @@ def auto_place_ship(ship_size, ship_number):
           temp_row2 = temp_point['loc']
           temp_col2 = temp_col
           direction = 'vertical'
-    
+        
         if check_ship(temp_row2, temp_col2) is False:
           ship_locations[ship_number][1] = [temp_row2, temp_col2]
           ship_segments -= 1
@@ -147,8 +168,8 @@ def auto_place_ship(ship_size, ship_number):
       else:
         logging.debug('Coords are NOT valid on board')
         continue
-        
-    ## all other subsequent segments 
+    
+    ## all other subsequent segments
     else:
       if direction == 'horizontal':
         logging.debug('temp_col2: {} temp_col: {}'.format(temp_col2,temp_col))
@@ -189,10 +210,9 @@ def auto_place_ship(ship_size, ship_number):
       else:
         logging.debug('Coords are NOT valid on board')
         continue
-        
+  
   logging.debug('Finished placing ship: {}'.format(ship_locations[ship_number]))
   logging.debug('Time to place ship: {}ms'.format((time.clock() - start)*1000))
-
 
 ##---------- check ship health -------------##
 def check_ship_health(guess_row, guess_col):
@@ -222,7 +242,7 @@ def input_coords():
         continue
       else:
         guess_row = int(guess_row)-1
-        guess_col = int(row_headers.index(guess_col.upper()))
+        guess_col = int(column_headers.index(guess_col.upper()))
     except ValueError:
       print 'Invalid row or column. For example enter 4C'
       continue
@@ -242,35 +262,35 @@ def input_coords():
 def play_game():
   global num_turns, guess_row, guess_col
   create_blank_game_board(difficulty[game_level])
-
+  
   logging.debug('There are a total of {} ships'.format(len(ships)))
   start = time.clock()
-
+  
   ##---- place all ships and map to board ----##
   for x in range(len(ships)):
      logging.debug("{} out of {} ships have been placed".format(x,len(ships)))
      auto_place_ship(ships[x],x)
-
+     
      for y in range(len(ship_locations[x])):
       ship_board[ship_locations[x][y][0]][ship_locations[x][y][1]] = str(x+1)
-
+  
   logging.debug('Time to place all ships: {}ms'.format((time.clock() - start)*1000))
   #print_board(ship_board) ##--- CHEATING! Prints game board with ship locations ----##
-
+  
   print "\nLet's play Battleship!\n"
   print_board(board)
-
+  
   while num_turns != 0:
     
     ship_hit = None
     print "\n{} remaining turns \n".format(num_turns)
-
+    
     input_coords()
-
+    
     if(board[guess_row][guess_col] == 'X' or board[guess_row][guess_col] == '*' ):
       print "\nYou guessed that one already."
       ship_hit = False
-
+    
     else:
       for x in range(len(ship_locations)):
         if [guess_row, guess_col] in ship_locations[x].values():
@@ -284,18 +304,19 @@ def play_game():
       print '\nMiss!'
       board[guess_row][guess_col] = "X"
       num_turns -= 1
-
+    
     print_board(board)
-
+  
   print 'Game Over\n'
   print 'Here\'s where all the ships were\n'
   print_board(ship_board)
 
+##------------------------------------------##
 def menu():
   global game_level, game_mode
-
+  
   print '\nWelcome to Battleship!\n'
-
+  
   if args.mode is None:
     print 'Select game mode:\n'
     print 'PVP - Player vs. Player'
@@ -303,21 +324,20 @@ def menu():
     game_mode = raw_input('> ').upper()
   else:
     game_mode = args.mode.upper()
-
+  
   if game_mode == 'PVE':
     if args.difficulty is None:
       game_level = raw_input('\nWhat level do you want to play (easy, medium or hard)? ').upper()
     else:
       game_level = args.difficulty.upper()
     play_game()
-
+  
   elif game_mode == 'PVP':
     print 'PVP mode'
     pass
 
+
 menu()
 
-
 ##------------- to do list ----------------------##
-## add more error checking to input data
 ## allow player to place ships
